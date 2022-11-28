bfa6cb2f336f3ff435ca0fdddfb3a98621b9c82a
Morning <morning@mstars-lab3-08s-iMac.local>
2022-11-28 13:22:52 +0800

    js

---
+++ variable.js
@@ -0,0 +1,85 @@
+//Task1,2
+//console.log('Hi developer', 'comment can make code readable')
+//Task3
+/*console.log('i can developer')*/
+//Task4,5
+var a=2;
+console.log(typeof a);
+var b=null;
+console.log(typeof b);
+var c="88"
+console.log(typeof c);
+var y;
+console.log(typeof y);
+var x=true;
+console.log(typeof x);
+//Task6
+var y;
+console.log(y);
+//Task7
+var w=7;
+console.log(w);
+//Task8
+var ner='Alex';
+var lastname='Liu';
+var country='UB';
+var age=23;
+console.log(ner);
+console.log(lastname);
+console.log(country);
+console.log(age);
+//Task9
+var ner='Alex', lastname='Liu', country='UB', Age=23;
+console.log(ner, lastname, Age, country);
+//Task11
+var a=7
+var b=9
+var t=5
+var q=a+b+t;
+console.log(q);
+//Task12
+var x=17
+var b=27
+var q=x*(2*b);
+console.log(q);
+//Task13
+var m=y*2-y*1/x*2-x*1;
+var y=77;
+var x=11;
+console.log(m);
+//Task16
+var a=1024;
+var b=15;
+var q=b*(a**3);
+console.log(q);
+//Task17
+var x=1024;
+var g=15;
+var q=(g+(x**2))/3;
+console.log(q);
+//Task18
+var side1=77;
+var side2=88;
+var side3=99;
+var q=(side1*side2)/2;
+console.log(q);
+
+console.log('Hi, Developers');
+let message = "hi";
+console.log('My name','hedy')
+let sum = 18+37;
+console.log(sum);
+let result = 60-16;
+console.log(result);
+let multiply =33*17;
+console.log(multiply);
+let percent =1200*0.2;
+console.log(percent);
+console.log(Number);
+{
+    var a=10;
+    var b=15;
+    var c=a+b;
+    console.log('addition of given 2 numbers is 25',c)
+    c=a+b;
+}
